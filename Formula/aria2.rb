799b3b9a38f0c06f671ef80dfeeda9d7b6b8ab03
OTSGE <199041525+otsge@users.noreply.github.com>
2025-11-03 02:36:03 +0000

    aria2: add 1.37.0 bottle.

--- Formula/aria2.rb
+++ Formula/aria2.rb
@@ -5,6 +5,16 @@ class Aria2 < Formula
   sha256 "60a420ad7085eb616cb6e2bdf0a7206d68ff3d37fb5a956dc44242eb2f79b66b"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/otsge/homebrew-stash2/releases/download/aria2-1.37.0"
+    sha256 cellar: :any,                 arm64_tahoe:   "19757a4ee17014b0f9eeba27c8e272320579533157f47157686a8002785805af"
+    sha256 cellar: :any,                 arm64_sequoia: "893dbcfddc6ea1dc6e80f2afb413f5f80d7e10514d40ef4ad53a8d2932438548"
+    sha256 cellar: :any,                 arm64_sonoma:  "ac120c8be89a6d3c3878ddcdbf537ffd846f0a3aa1edbef53c8402e38c0aecfe"
+    sha256 cellar: :any,                 sequoia:       "e06fed1a90699b2b7961e60b3ac3d0ade6a974ade1f8ac70cd9d510575799e56"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5309c307f52ff19194d003d3dbafb85373f842aa3925889db6a49b91489272af"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "37fc9a924da38f6819eb07ee5cecabe82c2ba66ba98abebb0475895a3274d952"
+  end
+
   head do
     url "https://github.com/aria2/aria2.git", branch: "master"
 
