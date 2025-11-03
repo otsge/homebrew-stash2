ee7a90939f266ccad4cfbca34a8657acb5621904
OTSGE <199041525+otsge@users.noreply.github.com>
2025-11-03 01:09:30 +0000

    Create otsge/stash2 tap

---
+++ Casks/stash-app.rb
@@ -0,0 +1,17 @@
+cask "stash-app" do
+  version "0.29.1"
+  sha256 "c2244a3caad8875c8c1c85bb05a30a490b0f679bce3586ae484d24bfdf95e148"
+
+  url "https://github.com/stashapp/stash/releases/download/v#{version}/Stash.app.zip",
+      verified: "github.com/stashapp/stash/"
+  name "Stash"
+  desc "Organizer for your porn, written in Go"
+  homepage "https://stashapp.cc/"
+
+  depends_on macos: ">= :big_sur"
+
+  app "Stash.app"
+
+  # zap trash: [
+  # ]
+end
---
+++ README.md
@@ -0,0 +1,18 @@
+# Otsge Stash2
+
+## How do I install these formulae?
+
+`brew install otsge/stash2/<formula>`
+
+Or `brew tap otsge/stash2` and then `brew install <formula>`.
+
+Or, in a `brew bundle` `Brewfile`:
+
+```ruby
+tap "otsge/stash2"
+brew "<formula>"
+```
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
