126169ab2468ac7ac7a850711fe333b00370824a
OTSGE <199041525+otsge@users.noreply.github.com>
2025-11-07 03:59:59 +0000

    stash-app 0.29.3

--- Casks/stash-app.rb
+++ Casks/stash-app.rb
@@ -1,6 +1,6 @@
 cask "stash-app" do
-  version "0.29.2"
-  sha256 "e4769b6921669a474e0e13ff1853e86721d049cd99be0906cc6c79f718a4e8f6"
+  version "0.29.3"
+  sha256 "a010afe8dfa87fedaa204211c1e48432700c37a3d2e87c81a1b5e80777012898"
 
   url "https://github.com/stashapp/stash/releases/download/v#{version}/Stash.app.zip",
       verified: "github.com/stashapp/stash/"
